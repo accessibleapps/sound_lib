ae5bc707877cacd7aec4479d83882988fdbb26ae
Christopher Toth <q.alpha@gmail.com>
2010-07-19 20:03:18 -0400

    Raise the proper exception when things go wrong...

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 from pybass import *
 from ctypes import *
 
+class BassError (Exception): pass
+
 def bass_call (function, *args):
  res = function(*args)
  if res == 0 or res == -1:
-  raise SoundOutputError(BASS_ErrorGetCode())
+  raise BassError(BASS_ErrorGetCode())
  return res

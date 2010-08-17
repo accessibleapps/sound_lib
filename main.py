e7ad9cdf5ccce12e256f5f84d3f99b8944f30fca
Q <q@qwitter-client.net>
2010-08-17 03:57:17 -0400

    Implemented a bass_call_0 function for items which can return 0 as a real value and not just an error.  Updated such items in channel.py

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from ctypes import *
 
 class BassError (Exception): pass
 
-def bass_call (function, *args):
+def bass_call(function, *args):
+ """Makes a call to bass and raises an exception if it fails."""
  res = function(*args)
  if res == 0 or res == -1:
   raise BassError(BASS_ErrorGetCode())
  return res
+
+def bass_call_0(function, *args):
+ """Makes a call to bass and raises an exception if it fails.  Does not consider 0 an error."""
+ res = function(*args)
+ if res == -1:
+  raise BassError(BASS_ErrorGetCode())
+ return res
+

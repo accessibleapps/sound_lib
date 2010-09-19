213a211efb523619ea825c73d279b578c7a2955a
Q <q@qwitter-client.net>
2010-09-19 02:07:24 -0400

    Implemented recording and recording to wave. Added is_playing, is_stopped, and is_paused methods to channels.

--- channel.py
+++ channel.py
@@ -19,6 +19,15 @@ class Channel (object):
   "Checks if a sample, stream, or MOD music is active (playing) or stalled. Can also check if a recording is in progress."""
   return bass_call_0(BASS_ChannelIsActive, self.handle)
 
+ def is_playing(self):
+  return self.is_active() == BASS_ACTIVE_PLAYING
+
+ def is_paused(self):
+  return self.is_active() == BASS_ACTIVE_PAUSED
+
+ def is_stopped(self):
+  return self.is_active() == BASS_ACTIVE_STOPPED
+
  def get_position (self, mode=BASS_POS_BYTE):
   """Retrieves the playback position of a sample, stream, or MOD music. Can also be used with a recording channel."""
   return bass_call_0(BASS_ChannelGetPosition, self.handle, mode)

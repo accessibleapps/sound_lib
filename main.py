c55115f4dc88d3c5c8e203afa833cc87f012fbf4
Christopher Toth <q.alpha@gmail.com>
2010-09-19 02:31:34 -0400

    Moved SoundInput and SoundOutput out of their own modules into main. I'll eventually scrap input and output entirely.

--- main.py
+++ main.py
@@ -27,3 +27,31 @@ def update_3d_system(func):
   return val
  update_wrapper(update_3d_system_wrapper, func)
  return update_3d_system_wrapper
+
+class SoundOutput(object):
+
+ def __init__ (self, device=-1, frequency=44100, flags=BASS_DEVICE_3D, window=0, clsid=None):
+  if platform.system() == 'Linux' and device == -1: #Bass wants default device set to 1 on linux
+   device = 1
+  BASS_Init(device, frequency, flags, window, clsid)
+
+ def get_volume (self):
+  volume = BASS_GetConfig(BASS_CONFIG_GVOL_STREAM)
+  if volume:
+   volume = volume / 100.0
+  return volume
+
+ def set_volume (self, volume):
+  #Pass in a float 0.0 to 100.0 and watch the volume magically change
+  bass_call(BASS_SetConfig, BASS_CONFIG_GVOL_STREAM, int(round(volume*100, 2)))
+
+ volume = property(fget=get_volume, fset=set_volume)
+
+
+class SoundInput(object):
+
+ def __init__ (self, device=-1):
+  if platform.system() == 'Linux' and device == -1: #Bass wants default device set to 1 on linux
+   device = 1
+  bass_call(BASS_RecordInit, device)
+

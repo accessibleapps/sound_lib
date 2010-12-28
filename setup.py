6aea7013f5d71ea54e1b398e7073cce48feb9716
Q <q@qwitter-client.net>
2010-12-28 18:21:35 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ import sys
 data_files = [['lib', glob('lib/*')]]
 if len(sys.argv) > 1 and (sys.argv[1] == 'bdist_wininst' or sys.argv[1] == 'bdist_msi'):
  for file in data_files:
-  file[0] = '\\PURELIB\\accessible_output\\%s' % file[0]
+  file[0] = '\\PURELIB\\sound_lib\\%s' % file[0]
 
 setup(
  name = 'sound_lib',
  author = 'Christopher Toth',
  author_email = 'q@qwitter-client.net',
- version = '0.4.0',
+ version = '0.5.0',
  url = 'http://www.qwitter-client.net',
  description = 'Pythonic wrapper to the Bass sound library',
  #long_description = open('README.txt').read(),

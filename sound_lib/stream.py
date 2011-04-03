0f164151bcad0f40cc1fe8613ea2ee0db502f08a
Christopher Toth <q.alpha@gmail.com>
2011-04-03 04:54:50 -0400

    Keep the file and urls of instantiated streams on their objects

--- sound_lib/stream.py
+++ sound_lib/stream.py
@@ -36,6 +36,7 @@ class FileStream(BaseStream):
  def __init__(self, mem=False, file=None, offset=0, length=0, flags=0, three_d=False, mono=False, autofree=False, decode=False):
   """Creates a sample stream from an MP3, MP2, MP1, OGG, WAV, AIFF or plugin supported file."""
   flags = flags | self.flags_for(three_d=three_d, autofree=autofree, mono=mono, decode=decode)
+  self.file = file
   handle = bass_call(BASS_StreamCreateFile, mem, file, offset, length, flags)
   super(FileStream, self).__init__(handle)
 
@@ -44,6 +45,7 @@ class URLStream(BaseStream):
  def __init__(self, url="", offset=0, flags=0, downloadproc=None, user=None):
   self._downloadproc = downloadproc or self._callback #we *must hold on to this
   self.downloadproc = DOWNLOADPROC(self._downloadproc)
+  self.url = url
   handle = bass_call(BASS_StreamCreateURL, url, offset, flags, self.downloadproc, user)
   super(URLStream, self).__init__(handle)
 

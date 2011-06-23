226cbcbd450e8a92c7d74bf0860d8291c5e47968
Q <q@qwitter-client.net>
2011-06-23 16:34:19 -0400

    attribute_map on channels and descendants modify. keys can be used as attributes to addjust in get/set/slide_attribute.

--- sound_lib/effects.py
+++ sound_lib/effects.py
@@ -5,12 +5,19 @@ from channel import Channel
 from main import bass_call, bass_call_0
 
 class Tempo(BaseStream):
-
+ 
  def __init__(self, channel, flags=0, loop=False, software=False, three_d=False, sample_fx=False, autofree=False, decode=False, free_source=False):
   flags = flags | self.flags_for(loop=False, software=False, three_d=False, sample_fx=False, autofree=False, decode=False, free_source=False)
   if isinstance(channel, Channel):
    channel = channel.handle
-  self.handle = bass_call(pybass_fx.BASS_FX_TempoCreate, channel, flags)
+  handle = bass_call(pybass_fx.BASS_FX_TempoCreate, channel, flags)
+  super(Tempo, self).__init__(handle)
+  self.add_attributes_to_mapping(
+   tempo=pybass_fx.BASS_ATTRIB_TEMPO,
+   tempo_pitch=pybass_fx.BASS_ATTRIB_TEMPO_PITCH,
+   tempo_freq=pybass_fx.BASS_ATTRIB_TEMPO_FREQ,
+  )
+ 
 
  @property
  def tempo(self):

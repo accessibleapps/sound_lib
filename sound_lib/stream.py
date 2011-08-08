f163c54ddae1c56de76fb1993cbb204908a7d9c0
Q <q@qwitter-client.net>
2011-08-08 18:14:41 -0400

    Refactored to use the bass plugin system for filetype detection instead of implementing our own filetype registry. This means URL streams work for all supported types.

--- sound_lib/stream.py
+++ sound_lib/stream.py
@@ -1,13 +1,6 @@
 from channel import Channel
 from main import bass_call
 from external.pybass import *
-from external.pybass_aac import *
-from external.pybassflac import *
-
-FILETYPE_HANDLERS = {
- '.aac': BASS_AAC_StreamCreateFile,
- '.flac': BASS_FLAC_StreamCreateFile,
-}
 
 class BaseStream(Channel):
 
@@ -34,12 +27,7 @@ class FileStream(BaseStream):
   self.setup_flag_mapping()
   flags = flags | self.flags_for(three_d=three_d, autofree=autofree, mono=mono, decode=decode, unicode=unicode)
   self.file = file
-  stream_creator = BASS_StreamCreateFile
-  for k in FILETYPE_HANDLERS:
-   if hasattr(file, 'lower') and file.lower().endswith(k):
-    stream_creator = FILETYPE_HANDLERS[k]
-    break
-  handle = bass_call(stream_creator, mem, file, offset, length, flags)
+  handle = bass_call(BASS_StreamCreateFile, mem, file, offset, length, flags)
   super(FileStream, self).__init__(handle)
 
  def setup_flag_mapping(self):

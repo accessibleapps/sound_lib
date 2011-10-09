bee84902fcc99f24f35f9cba87511af9ad09c85c
Q <q@qwitter-client.net>
2011-10-09 05:28:32 -0400

    Added a free method to the Input class which calls the appropriate BASS_RecordFree function.

--- sound_lib/input.py
+++ sound_lib/input.py
@@ -8,3 +8,7 @@ from main import bass_call
 class Input (object):
  def __init__ (self, device=-1):
   bass_call(BASS_RecordInit, device)
+
+ def free(self):
+  """Frees all resources used by the recording device."""
+  return bass_call(BASS_RecordFree)

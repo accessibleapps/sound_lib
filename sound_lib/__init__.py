f500e8c01dfc1efa51e0df76a824a60ef6a9817e
Q <q@qwitter-client.net>
2012-11-24 00:31:07 -0700

    Clenaed up library to not have so many top-level imports. Import things directly if you need them

--- sound_lib/__init__.py
+++ sound_lib/__init__.py
@@ -1,18 +1,7 @@
-from glob import glob
-import os
-from main import BassError
-import external #Trigger plugin loads
-
-import channel
-import effects
-import input
-import listener
-import music
-import output
-import recording
-import stream
 
 def py2exe_datafiles():
+ from glob import glob
+ import os
  import sound_lib
  path = os.path.join(sound_lib.__path__[0], 'lib')
  results = []

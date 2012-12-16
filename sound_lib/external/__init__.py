c8b2cf3ed06407252624c789d13b612d63c00c72
Q <q@qwitter-client.net>
2012-12-16 03:19:12 -0700

    function ref not call. fixed

--- sound_lib/external/__init__.py
+++ sound_lib/external/__init__.py
@@ -1,7 +1,7 @@
 import platform
 if platform.system() == 'Windows':
  import pybasswma
-if platform.system != 'Darwin':
+if platform.system() != 'Darwin':
  import pybass_aac
  import pybassflac
  import pybassmidi

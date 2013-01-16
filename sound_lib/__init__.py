fd0201de39c48bbe4e7e743eed1f5bc96bd83fb1
Christopher Toth <q.alpha@gmail.com>
2013-01-16 01:43:51 -0700

    Modified to find datafiles for all platforms

--- sound_lib/__init__.py
+++ sound_lib/__init__.py
@@ -1,12 +1,22 @@
+import platform
 
-def py2exe_datafiles():
+def find_datafiles():
  from glob import glob
  import os
  import sound_lib
  path = os.path.join(sound_lib.__path__[0], 'lib')
  results = []
+ system = platform.system()
+ if system == 'Windows':
+  file_ext = '*.dll'
+ elif system == 'Darwin':
+  file_ext = '*.dylib'
+ else:
+  file_ext = '*.so'
+
  for d in os.listdir(path):
   dest_dir = os.path.join('sound_lib', 'lib', d)
-  source = glob(os.path.join(path, d, '*.dll'))
+
+  source = glob(os.path.join(path, d, file_ext))
   results.append((dest_dir, source))
  return results

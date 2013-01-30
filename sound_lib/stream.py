1a6cc0473643698e483432aeb01be71dfb127a99
Christopher Toth <q.alpha@gmail.com>
2013-01-30 03:35:37 -0700

    More fighting with this damned filesystem encoding issue on OSX

--- sound_lib/stream.py
+++ sound_lib/stream.py
@@ -1,4 +1,5 @@
 import platform
+import sys
 from channel import Channel
 from main import bass_call
 from external.pybass import *
@@ -30,6 +31,9 @@ class FileStream(BaseStream):
 
  def __init__(self, mem=False, file=None, offset=0, length=0, flags=0, three_d=False, mono=False, autofree=False, decode=False, unicode=True):
   """Creates a sample stream from an MP3, MP2, MP1, OGG, WAV, AIFF or plugin supported file."""
+  if platform.system() == 'Darwin':
+   unicode = False
+   file = file.encode(sys.getfilesystemencoding())
   self.setup_flag_mapping()
   flags = flags | self.flags_for(three_d=three_d, autofree=autofree, mono=mono, decode=decode, unicode=unicode)
   if unicode and isinstance(file, str):
